d70f76244210bf2fdb7438334dad57b799c7c373
Oleksandr <oleksandr.kyrstiuk@gmail.com>
2026-08-15 18:28:20 +0300

    better light

--- Assets/Scripts/UI/TaskUI.cs
+++ Assets/Scripts/UI/TaskUI.cs
@@ -5,14 +5,32 @@ public class TaskUI : MonoBehaviour
 {
     [SerializeField] private TMP_Text[] taskTexts;
 
+    [Header("Current Task")]
+    [SerializeField] private Color currentTaskColor = new Color(1f, 0.9f, 0.7f);
+    [SerializeField] private float currentTaskFontSizeBonus = 2f;
+
+    private float defaultFontSize;
+    private Color defaultColor;
+
+    private void Awake()
+    {
+        if (taskTexts.Length == 0)
+            return;
+
+        defaultFontSize = taskTexts[0].fontSize;
+        defaultColor = taskTexts[0].color;
+    }
+
     public void UpdateTasks(Task[] tasks, int currentTaskIndex)
     {
         for (int i = 0; i < taskTexts.Length; i++)
         {
+            // Якщо для цього поля немає завдання
             if (i >= tasks.Length)
             {
                 taskTexts[i].text = "";
-                taskTexts[i].color = Color.white;
+                taskTexts[i].fontSize = defaultFontSize;
+                taskTexts[i].color = defaultColor;
                 taskTexts[i].fontStyle = FontStyles.Normal;
 
                 continue;
@@ -20,23 +38,28 @@ public class TaskUI : MonoBehaviour
 
             taskTexts[i].text = tasks[i].taskDescription;
 
+            // Виконане завдання
             if (i < currentTaskIndex)
             {
-                // Виконане завдання
+                taskTexts[i].fontSize = defaultFontSize;
+                taskTexts[i].color = defaultColor;
                 taskTexts[i].fontStyle = FontStyles.Strikethrough;
-                taskTexts[i].color = Color.white;
             }
+            // Поточне завдання
             else if (i == currentTaskIndex)
             {
-                // Поточне завдання
+                taskTexts[i].fontSize =
+                    defaultFontSize + currentTaskFontSizeBonus;
+
+                taskTexts[i].color = currentTaskColor;
                 taskTexts[i].fontStyle = FontStyles.Normal;
-                taskTexts[i].color = Color.green;
             }
+            // Майбутнє завдання
             else
             {
-                // Майбутнє завдання
+                taskTexts[i].fontSize = defaultFontSize;
+                taskTexts[i].color = defaultColor;
                 taskTexts[i].fontStyle = FontStyles.Normal;
-                taskTexts[i].color = Color.white;
             }
         }
     }
